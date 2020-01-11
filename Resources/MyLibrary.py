cd3881749e26aa295241eadd963559630b744cba
rcharaba <rodrigocharaba@hotmail.com>
2020-01-11 01:07:00 -0300

    new mouse actions

--- Resources/MyLibrary.py
+++ Resources/MyLibrary.py
@@ -6,7 +6,8 @@ pyautogui.position()
 def move_to(a1, a2):
     a = int(a1)
     b = int(a2)
-    pyautogui.moveTo(a, a)
+    #pyautogui.moveTo(a, b)
+    pyautogui.moveTo(a, b, duration=1)
 
 def three_arguments(a1, a2, a3):
     print("Keyword got three arguments '%s', '%s' and '%s'." % (a1, a2, a3))
